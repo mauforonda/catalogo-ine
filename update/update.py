92346be1b1ba4f27dc9622f158612d151029434b
Mauricio Foronda <josemauricioforonda@gmail.com>
2022-01-31 01:32:54 -0400

    small typo

--- update/update.py
+++ update/update.py
@@ -25,7 +25,7 @@ def update_catalogo(base):
     newlines = len(pd.concat([df.sort_values(['link']) for df in [catdf, old]]).drop_duplicates(keep=False))
     if newlines > 0:
         catdf = catdf[['modificado', 'nombre', 'tipo', 'kb', 'link']]
-        old_historial = pd.read_csv('catalogo_ine.csv', parse_dates=['modificado'])
+        old_historial = pd.read_csv('catalogo_ine_historial.csv', parse_dates=['modificado'])
         catdf_historial = pd.concat([old_historial, catdf]).drop_duplicates(subset=['modificado', 'nombre', 'tipo', 'link'], keep='first')
         catdf_historial.sort_values(['modificado', 'link']).to_csv('catalogo_ine_historial.csv', index=False, float_format="%.2f")
         catdf.sort_values(['modificado', 'link']).to_csv('catalogo_ine.csv', index=False, float_format="%.2f")
