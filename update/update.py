dd9dcc99d1c6c68362562c2079a7102a316fff30
Mauricio Foronda <josemauricioforonda@gmail.com>
2022-02-05 09:17:08 -0400

    Mejoras en la presentación de la tabla

--- update/update.py
+++ update/update.py
@@ -20,13 +20,13 @@ def update_catalogo(base):
 
     with open('update_time', 'w+') as f:
         f.write(requests.get('{}/update_time'.format(base)).text)
-    catdf = pd.read_csv('{}/catalogo_ine.csv'.format(base), parse_dates=['modificado'])
+    catdf = pd.read_csv('../catalogo_ine.csv'.format(base), parse_dates=['modificado'])
     old = pd.read_csv('catalogo_ine.csv', parse_dates=['modificado'])
     newlines = len(pd.concat([df.sort_values(['link']) for df in [catdf, old]]).drop_duplicates(keep=False))
     if newlines > 0:
-        catdf = catdf[['modificado', 'nombre', 'tipo', 'kb', 'link']]
+        catdf = catdf[['pagetitle', 'pagelink', 'modificado', 'nombre', 'tipo', 'kb', 'link']]
         old_historial = pd.read_csv('catalogo_ine_historial.csv', parse_dates=['modificado'])
-        catdf_historial = pd.concat([old_historial, catdf]).drop_duplicates(subset=['modificado', 'nombre', 'tipo', 'link'], keep='first')
+        catdf_historial = pd.concat([old_historial, catdf[['modificado', 'nombre', 'tipo', 'kb', 'link']]]).drop_duplicates(subset=['modificado', 'nombre', 'tipo', 'link'], keep='first')
         catdf_historial.sort_values(['modificado', 'link']).to_csv('catalogo_ine_historial.csv', index=False, float_format="%.2f")
         catdf.sort_values(['modificado', 'link']).to_csv('catalogo_ine.csv', index=False, float_format="%.2f")
 
